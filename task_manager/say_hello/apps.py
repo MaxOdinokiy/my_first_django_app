e3d6a5d195ecdb4971835c1886c7edd15d899d3a
Max Odinokiy <max.odinokiy@gmail.com>
2022-08-25 22:53:01 +0300

    Changed files structure

--- task_manager/say_hello/apps.py
+++ task_manager/say_hello/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyFirstSiteConfig(AppConfig):
+class SayHelloConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_first_site'
+    name = 'say_hello'
--- task_manager/say_hello/views.py
+++ task_manager/say_hello/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return  HttpResponse('Hello! Welcome to my first site!')
+    return HttpResponse('Hello! Here is my first web site!')
